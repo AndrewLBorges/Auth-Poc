61b0877d8fb23edc4e89151992d6249f72ca5890
Andrew Borges <andrew.borges@futurum.capital>
2024-08-07 16:18:53 -0300

    connecting to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './domain/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [SharedModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService
+          .get<string>('MONGO_DB_URL')
+          .replace('<PASSWORD>', configService.get('MONGO_DB_PASSWORD')),
+      }),
+      inject: [ConfigService],
+    }),
+    SharedModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/domain/users/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Role } from 'src/core/enums/role.enum';
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    select: false,
+  })
+  password: string;
+
+  @Prop({
+    required: true,
+    select: false,
+  })
+  confirmPassword: string;
+
+  @Prop({
+    required: true,
+  })
+  role: Role;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
